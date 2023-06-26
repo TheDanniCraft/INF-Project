36a2d85930c4d3256d521c68fb6093e2c0a40948
TheDanniCraft <mail@thedannicraft.de>
2023-06-26 14:17:27 +0200

    ✨Added saving / loading / importing / exporting

--- src/de/hws/fahrzeugverleih/app/AppManager.java
+++ src/de/hws/fahrzeugverleih/app/AppManager.java
@@ -1,24 +1,15 @@
 package de.hws.fahrzeugverleih.app;
 
-import java.awt.JobAttributes.DialogType;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.security.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import com.google.gson.*;
 import de.hws.fahrzeugverleih.data.Customer;
 import de.hws.fahrzeugverleih.data.Location;
 import de.hws.fahrzeugverleih.data.Motorcar;
 import de.hws.fahrzeugverleih.view.ViewManager;
+
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppManager {
 
@@ -51,9 +42,8 @@ public class AppManager {
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(allCars);
 			oos.close();
-		} 
-		catch (IOException e) {
-			ViewManager.getInstance().showMessage(e.getLocalizedMessage());
+		} catch (IOException ex) {
+			ViewManager.getInstance().showMessage(ex.getLocalizedMessage());
 		}
 	}
 	private void readCarsFromFile(String filePath) {
@@ -61,8 +51,7 @@ public class AppManager {
 			FileInputStream fis = new FileInputStream(filePath);
 			ObjectInputStream ois = new ObjectInputStream(fis);
 			allCars = (ArrayList<Motorcar>) ois.readObject();
-		} 
-		catch (IOException | ClassNotFoundException e) {
+		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
@@ -86,36 +75,114 @@ public class AppManager {
 		allCars = gson.fromJson(json, ArrayList.class);
 		} catch (IOException e) { e.printStackTrace(); }
 	}
-	
+
 	private void exportCarsToFile(String filePath) {
-		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		try {
+			gson.toJson(allCars, new FileWriter(filePath));
+		} catch (JsonIOException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 	private List<Motorcar> importCarsFromFile(String filePath) {
-		return null;
+		allCars = new ArrayList<Motorcar>();
+		try {
+			String resJSon = "";
+			InputStreamReader reader = new InputStreamReader(new FileInputStream(filePath), "UTF-8");
+			int data = reader.read();
+			while (data != -1) {
+				resJSon += (char) data;
+				data = reader.read();
+			}
+			reader.close();
+			JsonParser parser = new JsonParser();
+			JsonElement tree = parser.parse(resJSon);
+			JsonObject dataSet = tree.getAsJsonObject();
+			JsonElement elem = dataSet.get("cars");
+			if (elem.isJsonArray()) {
+				JsonArray datArr = elem.getAsJsonArray();
+				for (int i = 0; i < datArr.size(); i++) {
+					JsonObject obj = datArr.get(i).getAsJsonObject();
+					Motorcar car = new Motorcar(obj.get("fzid").getAsLong(), obj.get("license").getAsString(), obj.get("name").getAsString(), obj.get("type").getAsString(), obj.get("km").getAsInt(), Timestamp.valueOf(obj.get("nextcheck").getAsString()));
+					allCars.add(car);
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return allCars;
 	}
-	private void writeCustomerToFile(String filepath, List<Customer> cars) {
-		
+	private void writeCustomerToFile(String filePath) {
+		try {
+			FileOutputStream fos = new FileOutputStream(filePath);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(allCustomers);
+			oos.close();
+		} catch (IOException ex) {
+			ViewManager.getInstance().showMessage(ex.getLocalizedMessage());
+		}
 	}
-	private List<Customer> readCustomerFromFile(String filepath) {
-		return null;
+	private void readCustomerFromFile(String filePath) {
+		try {
+			FileInputStream fis = new FileInputStream(filePath);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			allCars = (ArrayList<Motorcar>) ois.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	private void exportCustomersToFile(String filePath) {
-		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		try {
+			gson.toJson(allCustomers, new FileWriter(filePath));
+		} catch (JsonIOException | IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-	private List<Customer> importCustomersToFile(String filePath) {
-		return null;
+	private List<Customer> importCustomersFromFile(String filePath) {
+		allCustomers = new ArrayList<Customer>();
+		try {
+			String resJSon = "";
+			InputStreamReader reader = new InputStreamReader(new FileInputStream(filePath), "UTF-8");
+			int data = reader.read();
+			while (data != -1) {
+				resJSon += (char) data;
+				data = reader.read();
+			}
+			reader.close();
+			JsonParser parser = new JsonParser();
+			JsonElement tree = parser.parse(resJSon);
+			JsonObject dataSet = tree.getAsJsonObject();
+			JsonElement elem = dataSet.get("customers");
+			if (elem.isJsonArray()) {
+				JsonArray datArr = elem.getAsJsonArray();
+				for (int i = 0; i < datArr.size(); i++) {
+					JsonObject obj = datArr.get(i).getAsJsonObject();
+					Customer customer = new Customer(obj.get("id").getAsLong(), obj.get("name").getAsString(), obj.get("vorname").getAsString(), obj.get("adresse").getAsString());
+					allCustomers.add(customer);
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return allCustomers;
 	}
 	public void readData() {
-		
+		readCarsFromFile("./Cars");
+		readCustomerFromFile("./Customers");
 	}
 	public void saveData() {
-		
+		writeCarsToFile("./Cars");
+		writeCustomerToFile("./Customers");
 	}
 	public void importData() {
-		
+		readCarsFromFile("./CarsDump.json");
+		readCustomerFromFile("./CustomersDump.json");
 	}
 	public void exportData() {
-		
+		exportCarsToFile("./CarsDump.json");
+		exportCustomersToFile("./CustomersDump.json");
 	}
 	public List<Motorcar> getCars() {
 		return null;
